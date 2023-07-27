b95eb5e88805b596758e4b51575b9ff5786345c0
Nir Tamir <nirtamir2@gmail.com>
2023-07-27 21:39:01 +0300

    simplify

--- views/useCurrentSlideNumber.ts
+++ views/useCurrentSlideNumber.ts
@@ -2,11 +2,5 @@ import { useContext } from "react";
 import { CurrentSlideNumberContext } from "./CurrentSlideNumberContext";
 
 export const useCurrentSlideNumber = (): number => {
-	const value = useContext(CurrentSlideNumberContext);
-	if (value == null) {
-		throw new Error(
-			"useCurrentSlideNumber - context CurrentSlideNumber is not initialized",
-		);
-	}
-	return value;
+	return useContext(CurrentSlideNumberContext);
 };
