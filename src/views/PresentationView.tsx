dec92ede8a6a874926255305b559070bc607a12b
Nir Tamir <nirtamir2@gmail.com>
2023-07-28 01:04:09 +0300

    check if server need restart

--- src/views/PresentationView.tsx
+++ src/views/PresentationView.tsx
@@ -1,9 +1,30 @@
-import { useContext } from "solid-js";
+import {
+	Show,
+	Suspense,
+	createEffect,
+	createResource,
+	on,
+	useContext,
+} from "solid-js";
 import { SlidevStoreContext } from "./SlidevStoreContext";
 import { useApp } from "./useApp";
 import { useSettings } from "./useSettings";
 
 const localhost = () => "localhost"; //`127.0.0.1`;
+
+async function fetchIsServerUp(serverAddr: string): Promise<boolean> {
+	try {
+		const response = await fetch(`${serverAddr}index.html`);
+		try {
+			await response.text();
+			return true;
+		} catch {
+			return false;
+		}
+	} catch {
+		return false;
+	}
+}
 
 export const PresentationView = () => {
 	const { vault } = useApp();
@@ -12,35 +33,21 @@ export const PresentationView = () => {
 
 	const serverAddr = () => `http://${localhost()}:${config.port}/`;
 
-	// const indexUrl = `${serverAddr}index.html`
-	// const resolvedBody = await got.get(indexUrl, { responseType: 'text', resolveBodyOnly: true }).catch(() => null)
-	// if (!resolvedBody) {
-	// 	serverAddr = `http://[::1]:${config.port}/`
-	// }
-	const url = () =>
-		`${serverAddr()}${store.currentSlideNumber}?embedded=true`;
+	const [isServerUp, { refetch }] = createResource(
+		serverAddr,
+		fetchIsServerUp,
+	);
 
-	// const getSlideNumber = () => {
-	// 	const activeEditor = workspace.getActiveViewOfType(MarkdownView);
-	// 	if (activeEditor == null) {
-	// 		console.log("null");
-	// 		return 1;
-	// 	}
-	//
-	// 	const cursor = activeEditor.editor.getCursor();
-	// 	console.log("activeEditor.data", activeEditor.data);
-	// 	const { line } = cursor;
-	// 	console.log("line", line);
-	// 	activeEditor.data;
-	// 	const noteFile = workspace.getActiveFile(); // Currently Open Note
-	// 	if (noteFile == null) {
-	// 		return 1;
-	// 	}
-	// 	if (!noteFile.name) return; // Nothing Open
-	//
-	// 	// Read the currently open note file. We are reading it off the HDD - we are NOT accessing the editor to do this.
-	// 	// 		const text = await vault.read(noteFile);
-	// };
+	createEffect(() => {
+		on(
+			() => store.currentSlideNumber,
+			() => void refetch(),
+		);
+	});
+
+	const url = () => {
+		return `${serverAddr()}${store.currentSlideNumber}?embedded=true`;
+	};
 
 	return (
 		<>
@@ -53,18 +60,26 @@ export const PresentationView = () => {
 			}
 			`}
 			</style>
-			<div class="container">
-				<h4>
-					{vault.getName()} #{store.currentSlideNumber}
-				</h4>
-				<iframe
-					sandbox
-					title="Slidev presentation"
-					style={{ height: "100%", width: "100%" }}
-					id="iframe"
-					src={url()}
-				/>
-			</div>
+			<Suspense fallback={<div class="container">Loading</div>}>
+				<Show
+					when={isServerUp()}
+					fallback={<div class="container">Server is down</div>}
+				>
+					<div class="container">
+						<h4>
+							{vault.getName()} #{store.currentSlideNumber}
+						</h4>
+						{/* eslint-disable-next-line react/iframe-missing-sandbox */}
+						<iframe
+							sandbox="allow-scripts allow-same-origin"
+							title="Slidev presentation"
+							style={{ height: "100%", width: "100%" }}
+							id="iframe"
+							src={url()}
+						/>
+					</div>
+				</Show>
+			</Suspense>
 		</>
 	);
 };
