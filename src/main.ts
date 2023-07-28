ed3d195c4fde1b77a677bfd2468c8b59ddcb60b9
Nir Tamir <nirtamir2@gmail.com>
2023-07-28 03:22:52 +0300

    remove stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 // import { createServer, resolveOptions } from "@slidev/cli";
 import { parse } from "@slidev/parser";
-import type { Editor } from "obsidian";
 import { MarkdownView, Plugin } from "obsidian";
-import { SampleModal } from "./SampleModal";
 import type { SlidevPluginSettings } from "./SlidevSettingTab";
 import { DEFAULT_SETTINGS, SlidevSettingTab } from "./SlidevSettingTab";
 import "./styles.css";
@@ -42,55 +40,25 @@ export default class SlidevPlugin extends Plugin {
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
+			"presentation",
 			"Open Slidev Presentation View",
 			() => {
 				void this.activateView();
 			},
 		);
+
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
+		ribbonIconEl.addClass("slidev-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText("Status Bar Text");
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
+			id: "open-slidev--presentation-view",
+			name: "Open slidev presentation view",
 			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return void 0;
+				void this.activateView();
 			},
 		});
 
@@ -123,16 +91,6 @@ export default class SlidevPlugin extends Plugin {
 			// console.log("view.editor.getValue()", text);
 			// console.log({ slideIndex, line, currentSlide });
 		});
-		//
-		// 	// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// 	this.registerInterval(
-		// 		window.setInterval(
-		// 			() => {
-		// 				console.log("setInterval");
-		// 			},
-		// 			5 * 60 * 1000,
-		// 		),
-		// 	);
 
 		if (import.meta.env.DEV) {
 			window.hmr(this);
@@ -185,6 +143,7 @@ export default class SlidevPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		// TODO: do it more elegantly instead of unload and load
 		this.onunload();
 		void this.onload();
 	}
